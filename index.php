251f5d532bdaf1d30bf0fecb666f2b596c78561b
janQuart <janquart@fsj.org>
2021-06-25 14:15:20 -0500

    here are some notes

--- index.php
+++ index.php
@@ -10,7 +10,10 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="https://www.touchstonemag.com/bs5/conference.css" />
-    
+    <!--Jerry has added some serious notage here-->
+    <!--Jerry has added some serious notage here-->
+    <!--Jerry has added some serious notage here-->
+    <!--Jerry has added some serious notage here-->
     <meta property="og:type" content="article" />
 	<meta property="og:image" content="https://www.touchstonemag.com/conference/social-pic.png" />
 	<meta property="og:site_name" content="Touchstone: A Journal of Mere Christianity"/>
