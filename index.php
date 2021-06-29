2a51c69536087ea460a38e5c6799733684ad2d83
janQuart <janquart@fsj.org>
2021-06-29 09:54:19 -0500

    Change css link

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet"> 
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://www.touchstonemag.com/bs5/conference.css" />
+    <link rel="stylesheet" href="https://www.touchstonemag.com/conference/conference.css" />
     
     <meta property="og:type" content="article" />
 	<meta property="og:image" content="https://www.touchstonemag.com/conference/social-pic.png" />
